321e990e6bd0ad425b43d19eb7984e55774c5e81
jcmm <jcmmelton@gmail.com>
2018-09-29 13:03:42 -0400

    flyaround camera working

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,15 @@ extern crate shader_version;
 extern crate glutin_window;
 extern crate gl;
 extern crate nalgebra as na;
-extern crate camera_controllers;
+extern crate nalgebra_glm as glm;
 
 
 mod vertex;
 mod cube;
 
 use glium::*;
-use na::{Matrix4, geometry, Vector3, Vector2, Isometry3, Point3, Perspective3};
+use na::{Matrix4, geometry, Vector3, Vector2, Isometry3, Point3, Perspective3, Rotation3};
+use glm::*;
 use std::f32;
 use vertex::Vertex;
 use cube::*;
@@ -30,17 +31,18 @@ fn main() {
     let display: Display = glium::Display::new(window, context, &event_loop).unwrap();
     display.gl_window().hide_cursor(true);
 
-    let light_position: (f32, f32, f32) = (0.0, 10.0, 5.0);
+    let light_position: (f32, f32, f32) = (0.0, 0.0, 15.0);
     let cube_iter = 10;
     let mut cubes: Vec<Cube> = Vec::new();
     for i in -cube_iter..cube_iter {
         for j in -cube_iter..cube_iter {
             let fi = i as f32;
             let fj = j as f32;
+            let hp = 1.0/hypot(fi, fj);
             cubes.push(Cube::new(
                 CubeType::Block,
                 Vector3::new(fi*1.0, fj*1.0, 10.0),
-                [f32::abs(1.0/fi), f32::abs(1.0/fj), 0.3],
+                [hp, hp, 0.3],
                 0.5,
                 &display
             ));
@@ -66,23 +68,6 @@ fn main() {
 
     let projection = geometry::Perspective3::new(dimensions[0]/dimensions[1], f32::consts::PI/2.0, 0.1, 1000.0);
 
-
-    // let player_pos = [0.0, 0.0, 0.0];
-    // let player_yaw = 0.0;
-    // let player_pitch = 0.0;
-    // let mut first_person_settings = camera_controllers::FirstPersonSettings::keyboard_wasd();
-    // first_person_settings.mouse_sensitivity_horizontal = 0.5;
-    // first_person_settings.mouse_sensitivity_vertical = 0.5;
-    // first_person_settings.speed_horizontal = 8.0;
-    // first_person_settings.speed_vertical = 4.0;
-    // let mut first_person = camera_controllers::FirstPerson::new(
-    //     player_pos,
-    //     first_person_settings
-    // );
-    // first_person.yaw = f32::consts::PI - player_yaw / 180.0 * f32::consts::PI;
-    // first_person.pitch = player_pitch / 180.0 * f32::consts::PI;
-
-
     let mut closed = false;
     let mut d: f32 = 0.001;
 
@@ -102,20 +87,49 @@ fn main() {
     };
     let mut rotate_cubes: bool = false;
     let mut mouse_position: Vector2<f32> = Vector2::new(0.0, 0.0);
+    let mut mouse_offset: Vector2<f32> = Vector2::new(0.0, 0.0);
+    let mouse_sensitivity: f32 = 0.5;
+    let mut pitch: f32 = 0.0;
+    let mut yaw: f32 = 0.0;
+
+    let camera_speed: f32 = 1.0;
+    let mut camera_pos = glm::vec3(0.0, 0.0, 3.0);
+    let mut camera_front = glm::vec3(0.0, 0.0, 1.0);
+    let camera_up  = glm::vec3(0.0, 1.0, 0.0);
 
     while !closed {
 
         let mut target = display.draw();
         target.clear_color_and_depth((0.01, 0.01, 0.01, 1.0), 1.0);
 
-        let view:  Matrix4<f32> = Isometry3::look_at_rh(&eye, &targ, &Vector3::y()).to_homogeneous();
+        yaw   += mouse_offset.x;
+        pitch += mouse_offset.y;
+        if pitch > 89.0 {
+            pitch = 89.0;
+        }
+        if pitch < -89.0 {
+            pitch = -89.0;
+        }
+
+        let front = glm::vec3(
+            f32::cos(radianize(&yaw))* f32::cos(radianize(&pitch)),
+            f32::sin(radianize(&pitch)),
+            f32::sin(radianize(&yaw)) * f32::cos(radianize(&pitch))
+        );
+        camera_front = glm::normalize(&front);
+
+        let view = glm::look_at(
+            &camera_pos,
+            &(camera_pos+camera_front),
+            &camera_up
+        );
 
         for cube in cubes.iter_mut() {
 
             if rotate_cubes {
                 match cube.get_type() {
                     CubeType::Block => {
-                        let f = 1.0 + f32::sqrt( f32::powf(cube.get_x_pos(), 2.0) + f32::powf(cube.get_y_pos(), 2.0));
+                        let f = 1.0 + hypot(cube.get_x_pos(),cube.get_y_pos());
                         cube.rotate(f32::cos(d), f32::sin(d), d/f);
                         cube.move_location(Vector3::new(0.0, 0.0, f32::sin(d+f)/100.0));
                     },
@@ -140,8 +154,20 @@ fn main() {
         }
         target.finish().unwrap();
 
+        mouse_offset.x = 0.0;
+        mouse_offset.y = 0.0;
+
         event_loop.poll_events(|event| {
             match event {
+                glutin::Event::DeviceEvent { event, ..} => match event {
+                    glutin::DeviceEvent::MouseMotion{delta}  => match delta {
+                        _ => {
+                            mouse_offset.x = ( delta.0 as f32) * mouse_sensitivity;
+                            mouse_offset.y = (-delta.1 as f32) * mouse_sensitivity;
+                        }
+                    },
+                    _ => ()
+                }
                 glutin::Event::WindowEvent { event, .. } => match event {
                     glutin::WindowEvent::CloseRequested => closed = true,
                     glutin::WindowEvent::Resized(size) => {
@@ -150,20 +176,16 @@ fn main() {
                     },
                     glutin::WindowEvent::KeyboardInput{device_id, input} => match input.virtual_keycode {
                         Some(glutin::VirtualKeyCode::W) => {
-                            eye.z  += 0.1;
-                            targ.z += 0.1;
-                        }
+                            camera_pos += camera_speed * camera_front;
+                        },
                         Some(glutin::VirtualKeyCode::S) => {
-                            eye.z  -= 0.1;
-                            targ.z -= 0.1;
+                            camera_pos -= camera_speed * camera_front;
                         },
                         Some(glutin::VirtualKeyCode::D) => {
-                            eye.x  -= 0.1;
-                            targ.x -= 0.1;
+                            camera_pos += glm::normalize(&glm::cross::<f32, U3>(&camera_front, &camera_up)) * camera_speed;
                         },
                         Some(glutin::VirtualKeyCode::A) => {
-                            eye.x  += 0.1;
-                            targ.x += 0.1;
+                            camera_pos -= glm::normalize(&glm::cross::<f32, U3>(&camera_front, &camera_up)) * camera_speed;
                         },
                         Some(glutin::VirtualKeyCode::R) => {
                             rotate_cubes = !rotate_cubes;
@@ -173,55 +195,56 @@ fn main() {
                         },
                         _ => println!("{:?}", input)
                     }
-                    glutin::WindowEvent::CursorMoved{device_id, position, modifiers} => match position {
-                        _ => {
-                            mouse_position.x = position.x as f32;
-                            mouse_position.y = position.y as f32;
-                        }
-                    }
                     _ => ()
                 },
                 _ => (),
             }
-        // first_person.event(&event);
         });
         d += 0.01;
     }
 
 }
 
-// fn get_x_rot(d: f32) -> Matrix4<f32> {
-//     let cd = f32::cos(d);
-//     let sd = f32::sin(d);
-//     Matrix4::new(
-//         1.0, 0.0, 0.0, 0.0,
-//         0.0,  cd, -sd, 0.0,
-//         0.0,  sd,  cd, 0.0,
-//         0.0, 0.0, 0.0, 1.0
-//     )
-// }
+fn radianize(n: &f32) -> f32 {
+    n * (f32::consts::PI/180.0)
+}
 
-// fn get_y_rot(d: f32) -> Matrix4<f32> {
-//     let cd = f32::cos(d);
-//     let sd = f32::sin(d);
-//     Matrix4::new(
-//          cd, 0.0,  sd, 0.0,
-//         0.0, 1.0, 0.0, 0.0,
-//         -sd, 0.0,  cd, 0.0,
-//         0.0, 0.0, 0.0, 1.0
-//     )
-// }
+fn hypot(a: f32, b: f32) -> f32 {
+    f32::sqrt(f32::powi(a, 2) + f32::powi(b, 2))
+}
 
-// fn get_z_rot(d: f32) -> Matrix4<f32> {
-//     let cd = f32::cos(d);
-//     let sd = f32::sin(d);
-//     Matrix4::new(
-//          cd, -sd, 0.0, 0.0,
-//          sd,  cd, 0.0, 0.0,
-//         0.0, 0.0, 1.0, 0.0,
-//         0.0, 0.0, 0.0, 1.0
-//     )
-// }
+//fn get_x_rot(d: f32) -> Matrix4<f32> {
+//    let cd = f32::cos(d);
+//    let sd = f32::sin(d);
+//    Matrix4::new(
+//        1.0, 0.0, 0.0, 0.0,
+//        0.0,  cd, -sd, 0.0,
+//        0.0,  sd,  cd, 0.0,
+//        0.0, 0.0, 0.0, 1.0
+//    )
+//}
+//
+//fn get_y_rot(d: f32) -> Matrix4<f32> {
+//    let cd = f32::cos(d);
+//    let sd = f32::sin(d);
+//    Matrix4::new(
+//         cd, 0.0,  sd, 0.0,
+//        0.0, 1.0, 0.0, 0.0,
+//        -sd, 0.0,  cd, 0.0,
+//        0.0, 0.0, 0.0, 1.0
+//    )
+//}
+//
+//fn get_z_rot(d: f32) -> Matrix4<f32> {
+//    let cd = f32::cos(d);
+//    let sd = f32::sin(d);
+//    Matrix4::new(
+//         cd, -sd, 0.0, 0.0,
+//         sd,  cd, 0.0, 0.0,
+//        0.0, 0.0, 1.0, 0.0,
+//        0.0, 0.0, 0.0, 1.0
+//    )
+//}
 
 // fn get_identity_matrix() -> Matrix4<f32> {
 //     Matrix4::new(
